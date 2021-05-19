ec59cf23ab612be1ee7e346badfd9072aa3136f9
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-05-19 23:24:08 +1000

    Добавил пример использования интерфейсов IEnumerable и IEnumerator

---
+++ 01_UserColletions/UserCollections/Tut_02_IEnumerableIEnumerator_01/Collection/UserCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace Tut_02_IEnumerableIEnumerator_01.Collection
+{
+    public class UserCollection: IEnumerable, IEnumerator, IDisposable
+    {
+        // Поле
+        private readonly Element[] _elements = new Element[4];
+
+        // Индексатор
+        public Element this[int index]
+        {
+            get { return _elements[index]; }
+            set { _elements[index] = value; }
+        }
+
+        /// <summary>
+        /// Указатель текущего элемента в коллекции
+        /// </summary>
+        private int _position = -1;
+        
+        // Реализация интерфейса IEnumerator:
+        #region IEnumerator
+        
+        // Метод MoveNext()
+        public bool MoveNext()
+        {
+            if (_position < _elements.Length - 1)
+            {
+                _position++;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Метод Reset
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        // Свойство Current
+        public object Current
+        {
+            get { return _elements[_position]; }
+        }
+        
+        #endregion
+
+        // Реализация интерфейса IEnumerable
+        #region IEnumerable
+        
+        /// <summary>
+        /// Возвращаяет ссылку на енумератор
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator) this;
+        }
+        
+        #endregion
+
+
+        public void Dispose()
+        {
+            ((IEnumerator)this).Reset();
+        }
+    }
+}
---
+++ 01_UserColletions/UserCollections/Tut_02_IEnumerableIEnumerator_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Reflection.Emit;
+using Tut_02_IEnumerableIEnumerator_01.Collection;
+
+namespace Tut_02_IEnumerableIEnumerator_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new UserCollection();
+
+            collection[0] = new Element(1, 2);
+            collection[1] = new Element(3, 4);
+            collection[2] = new Element(5, 6);
+            collection[3] = new Element(7, 8);
+            
+            Console.WriteLine("Перебор через foreach");
+
+            foreach (Element element in collection)
+            {
+                Console.WriteLine("{0}, {1}", element.FieldA, element.FieldB);
+            }
+            
+            Console.WriteLine(new string('-', 5));
+
+            // Код аналогичен тому, который использует компилятор при преобразовании foreach
+            Console.WriteLine("Ручной перебор коллекции:");
+
+            IEnumerator enumerator = ((IEnumerable)collection).GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Element element = (Element)enumerator.Current;
+                
+                Console.WriteLine("{0}, {1}", element.FieldA, element.FieldB);
+            }
+
+            ((IDisposable)collection).Dispose();
+            
+            Console.ReadLine();
+        }
+    }
+}
